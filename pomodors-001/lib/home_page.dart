37fd97f5a88595c39434fa4356342df146e0def8
n96js <n96js@naver.com>
2024-03-08 10:49:18 +0900

    timer

--- pomodors-001/lib/home_page.dart
+++ pomodors-001/lib/home_page.dart
@@ -3,7 +3,10 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  // state에게 보내주려면 this 키워드를 붙여줘야함
+  const HomePage({super.key, required this.workTimer});
+
+  final int workTimer;
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -20,9 +23,30 @@ class _HomePageState extends State<HomePage> {
   /// 이럴때 late 라는 키워드를 부착하여 조금있다가 아래 코드에서
   /// 변수를 꼭!! 초기화 할테니 일단 보류 해달라 라는 의미
   late Timer _timer;
-  static const int wantTimer = 10;
-  int _count = wantTimer;
+
+  /// main 으로부터 전달받은 workTimer
+  /// workTimer 는 setting_page 에서 일할 시간을 변경하면 그 값을 전달해주는
+  /// 전달자 state 이다.
+  /// 즉, setting_page 에서 일할시간을 변경하면 timer 의 전체시간이 변경되어야 한다.
+  /// 일반적인 코드 형식이라면
+  /// int_count = workTimer; 이라고 사용한다.
+  /// 하지만 Flutter 의 State<> 에서는
+  /// int _count = widget.workTimer 라고 사용을 해야 한다.
+  /// 문제는 State<> class 영역에서는 widget 을 사용할 수 없다.
+  /// 따라서 _count 변수에 workTimer 값을 할당하기 위해서는
+  /// 별도의 방법을 사용해야 한다.
+  /// Flutter 의 State<> 클래스에는 initState() 라는 함수를 사용할 수 있다.
+  /// 이 함수에서 _count 변수에 widget.workTimer 값을 할당하는
+  /// 코드를 사용해야 한다.
+  int _count = 5;
   bool _timerRun = false;
+
+  /// State<> class 가 mount(화면이 다 그려졌을때) 될때 자동으로 실행되는 함수
+  @override
+  void initState() {
+    super.initState();
+    _count = widget.workTimer;
+  }
 
   void _onPressed() {
     setState(() {
@@ -37,7 +61,7 @@ class _HomePageState extends State<HomePage> {
           // -1씩 감소
           setState(() => _count--);
           if (_count < 1) {
-            _count = wantTimer;
+            _count = widget.workTimer;
             _timerRun = false;
             timer.cancel();
           }
--- pomodors-001/lib/setting_page.dart
+++ pomodors-001/lib/setting_page.dart
@@ -4,15 +4,34 @@ import 'package:settings_ui/settings_ui.dart';
 // 받을때는 statefulWidget이 받음
 class SettingPage extends StatefulWidget {
   // onchange 라는 함수를 반드시 전달받겠다.
-  const SettingPage({super.key, required this.onChange});
-  // 밑의 state에게 전달하기위해 한번더 세팅
-  final Function(String) onChange;
+  // required this.onChange
+  // main 으로부터 onChange 라는 props를 반드시 받도록 선언하기
+  const SettingPage(
+      {super.key, required this.onChange, required this.workTimer});
 
+  // 밑의 state에게 전달하기위해 한번더 세팅
+  // State 에서 onChange 를 사용할수 있도록 선언하기
+  // onChange 라는 props 는 함수이다(Functuin).
+  // 또한 문자열형 매개변수를 통하여 값을 전달할 수 있다.
+  // onChange 함수를 사용하는 곳에서 문자열을 전달하면
+  // 그 문자열이 main 으로 전달된다.
+  final Function(String) onChange;
+  final int workTimer;
   @override
   State<SettingPage> createState() => _SettingPageState();
 }
 
 class _SettingPageState extends State<SettingPage> {
+  // id를 붙이기 위한 도구(controller)
+  final workTimeController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    // 설정탭에 들어가면 현재 설정된 값을 보여줌
+    workTimeController.text = widget.workTimer.toString();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,8 +43,21 @@ class _SettingPageState extends State<SettingPage> {
               SettingsTile(
                 leading: const Icon(Icons.timer_outlined),
                 title: TextField(
+                  /// TextField 에 id(ref) 부착하기
+                  controller: workTimeController,
                   // 사용하려면 widget. 을 써줌
                   // 키보드로 입력받은 문자열을 value에 담아 전달해서 main한테 감
+                  /// StateFul 에서 선언한 props 를 사용할때는
+                  /// widget.Props 형태로 사용한다.
+                  /// StateFul 에서 선언한 onChange 함수를
+                  /// widget.onChange() 라는 형식으로 사용한다.
+                  /// 만약 이 함수에 전달할 값이 필요 없다면
+                  /// onChange : widget.onChange 형식으로 사용하면 된다.
+                  /// 하지만 현재 widget.onChange 함수는 문자열형 매개변수를
+                  /// 필요로 하도록 선언되어 있다 : final Function(String) onChange
+
+                  /// 필요한 값을 전달하기 위하여 함수 선언형으로 event 핸들러를
+                  /// 사용한다.
                   onChanged: (value) => widget.onChange(value),
                   keyboardType: TextInputType.number,
                   decoration: const InputDecoration(
